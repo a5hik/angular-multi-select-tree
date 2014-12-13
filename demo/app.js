0f1aaf5b74e4fd75500438f324232ba83667f5cb
a5hik <ashikes@gmail.com>
2014-12-13 14:21:15 +0530

    package re strucutre.

--- demo/app.js
+++ demo/app.js
@@ -7,13 +7,4 @@ var app = angular.module('demoApp', ['multi-select']);
 
 app.controller('demoAppCtrl', function ($scope) {
 
-  $scope.inputList = [
-    { firstName: "Peter",    lastName: "Parker", selected: false },
-    { firstName: "Mary",     lastName: "Jane", selected: false },
-    { firstName: "Bruce",    lastName: "Wayne", selected: true  },
-    { firstName: "David",    lastName: "Banner", selected: false },
-    { firstName: "Natalia",  lastName: "Romanova", selected: false },
-    { firstName: "Clark",    lastName: "Kent", selected: true  }
-  ];
-
 });
