6a1fedf3a2b9aa1074652e985f9a505af6716e90
rajnikant-tripathi <rajnikant@mindigit.com>
2015-10-21 13:09:38 +0530

    selected Item does not change
    Currently if I change the selected node from outside, it does not change the selected node. To achieve the same , we need to watch the whole property of "inputModel" and not just reference. Please accept this pull request to avoid me putting a hack in my project:)

--- src/multi-select-tree.js
+++ src/multi-select-tree.js
@@ -235,7 +235,7 @@
               scope.refreshSelectedItems();
               scope.refreshOutputModel();
             }
-          });
+          }, true);
 
           /**
            * Checks whether any of children match the keyword.
